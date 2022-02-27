5bb4b4c11947d4394383b9faeaae8867d198b93a
karthiksuresh181 <karthu.karthu1@gmail.com>
2022-02-27 11:32:10 +0530

    Init

---
+++ src/assets/theme-dark/components/form/textField.js
@@ -0,0 +1,12 @@
+// Material Dashboard 2 PRO React Base Styles
+import colors from "assets/theme-dark/base/colors";
+
+const { transparent } = colors;
+
+export default {
+  styleOverrides: {
+    root: {
+      backgroundColor: transparent.main,
+    },
+  },
+};
---
+++ src/routes.js
@@ -0,0 +1,25 @@
+// Material Dashboard 2 React layouts
+import Dashboard from "layouts/dashboard";
+// import Tables from "layouts/tables";
+// import Billing from "layouts/billing";
+// import RTL from "layouts/rtl";
+// import Notifications from "layouts/notifications";
+// import Profile from "layouts/profile";
+// import SignIn from "layouts/authentication/sign-in";
+// import SignUp from "layouts/authentication/sign-up";
+
+// @mui icons
+import Icon from "@mui/material/Icon";
+
+const routes = [
+  {
+    type: "collapse",
+    name: "Dashboard",
+    key: "dashboard",
+    icon: <Icon fontSize="small">dashboard</Icon>,
+    route: "/tranalyze-frontend-material-dashboard/",
+    component: <Dashboard />,
+  },
+];
+
+export default routes;
